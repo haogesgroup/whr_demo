92168d93237d39c4c691d7d2ea26a45c78ce00af
haoge <wuhr@lianjintai.com>
2019-08-13 13:56:03 +0800

    whr_demo inii

---
+++ src/main/java/com/test/whr_demo/WhrDemoApplication.java
@@ -0,0 +1,13 @@
+package com.test.whr_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhrDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhrDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/whr_demo/WhrDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.test.whr_demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhrDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
